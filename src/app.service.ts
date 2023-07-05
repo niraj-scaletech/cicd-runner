07c1ea862b72b49b9e540292f3cfc8f155fad7a1
niraj-scaletech <niraj.visavadiya@scaletech.xyz>
2023-07-05 14:14:25 +0530

    Change response message

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Niraj Visavadiya';
+    return 'Hello Niraj';
   }
 }
